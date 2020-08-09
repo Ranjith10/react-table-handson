0737f2d5894f9ca288693348d3cfe7e6fc258c87
Ranjith kumar <ranjit4072@gmail.com>
2020-08-09 16:40:14 +0530

    Added 404 for error routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 import TableContent from './components/TableContent'
 import TableWrapper from './components/TableWrapper'
+import GenericNotFound from './GenericNotFound'
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <div className = 'app-container-subtitle'>Star Wars</div>
                 <Switch>
                     <Route exact path = '/'><TableContent /></Route>
+                    <Route component = { GenericNotFound } path = '*' />
                     <TableWrapper />
                 </Switch>
             </div>
---
+++ src/GenericNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GenericNotFound = () => {
+    return (
+        <div>
+            <img alt = 'not-found' src = '/images/notFound.jpg' />
+        </div>
+    )
+}
+
+export default GenericNotFound
